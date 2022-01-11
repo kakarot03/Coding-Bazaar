dd77585efcc1d7f45508c5deb6a91fa42452dde1
Srivarshan <srivarshan611@gmail.com>
2022-01-11 08:14:18 +0530

    added slides to the page

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const sliderItems = [{
+        id: 1,
+        img: require("./components/images/2.png"),
+        title: "SUMMER SALE",
+        desc: "Dont Compromise On Style, Get 40% Offer On New Arrivals",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: require("./components/images/1.png"),
+        title: "AUTUMN COLLECTION",
+        desc: "Dont Compromise On Style, Get 40% Offer On New Arrivals",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: require("./components/images/3.png"),
+        title: "WINTER EXCLUSIVE",
+        desc: "Dont Compromise On Style, Get 40% Offer On New Arrivals",
+        bg: "fbf0f4",
+    },
+]
+
+export const categories = [{
+        id: 1,
+        img: "",
+        title: "Shirt Style",
+    },
+    {
+        id: 2,
+        img: "",
+        title: "LoungWear Love",
+    },
+    {
+        id: 3,
+        img: "",
+        title: "Light Jackets",
+    },
+];
