3e462808f8ba72a3e73a506f85c2173484935a2a
Srivarshan <srivarshan611@gmail.com>
2022-01-13 09:53:26 +0530

    embedded a Product viewing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Home } from "./pages/Home";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Product />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Announcement from '../components/Announcement'
-import Categories from '../components/Categories'
-import Navbar from '../components/Navbar'
-import Sliders from '../components/Sliders'
-import Products from '../components/Products'
-import NewsLetter from '../components/NewsLetter'
+import React from "react";
+import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Navbar from "../components/Navbar";
+import Sliders from "../components/Sliders";
+import Products from "../components/Products";
+import NewsLetter from "../components/NewsLetter";
+import Footer from "../components/Footer";
 
 export const Home = () => {
-    return (
-        <div>
-            <Announcement />
-            <Navbar />
-            <Sliders/>
-            <Categories />
-            <Products />
-            <NewsLetter />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Sliders />
+      <Categories />
+      <Products />
+      <NewsLetter />
+      <Footer />
+    </div>
+  );
+};
