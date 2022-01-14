85d277ba7537b19229600277efae64b75dbb48f9
Sathya Prakaash <sathyaprakaash112@gmail.com>
2022-01-14 18:40:52 +0530

    Commit 2

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,22 +1,18 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Container = styled.div`
-    height : 30px;
-    background-color : teal;
-    display : flex;
-    align-items : center;
-    justify-content : center;
-    fonr-size : 14px;
-    font-weight : 500;
-    color : white;
-`
+  height: 30px;
+  background-color: teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+  color: white;
+`;
 
 const Announcement = () => {
-    return (
-        <Container>
-            Super Deal on Crocs !
-        </Container>
-    )
-}
+  return <Container>Sathya the mass</Container>;
+};
 
-export default Announcement
+export default Announcement;
