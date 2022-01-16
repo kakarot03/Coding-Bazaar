189c3cb923490f7e5e23b04ca5d6c5764d8bc60f
Sathya Prakaash <sathyaprakaash112@gmail.com>
2022-01-16 23:53:13 +0530

    Commit check

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Home } from "./pages/Home";
 import Product from "./pages/Product";
-
+import SignIn from "./pages/SignIn";
 function App() {
   return (
     <div className="App">
-      <Product />
+      <SignIn />
     </div>
   );
 }
