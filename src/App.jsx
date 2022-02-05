5a4da48db2f913f0378feb51355a9d29e604c6a4
Sathya Prakaash <sathyaprakaash112@gmail.com>
2022-02-05 23:00:06 +0530

    Test commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Product from "./pages/Product";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignIn";
 import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Products from "./components/Products";
+
 function App() {
   return (
     <div className="App">
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -159,3 +159,4 @@ export const Login = () => {
     </Container>
   );
 };
+export default Login;
